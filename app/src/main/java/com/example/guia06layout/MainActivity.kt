6d102ace44df3ebf730ec7d2856d2d9a4d5233f8
MarvinArtiga <marvin.artigagonzalez@gmail.com>
2025-08-22 08:18:11 -0600

    Terminada guia06

--- app/src/main/java/com/example/guia06layout/MainActivity.kt
+++ app/src/main/java/com/example/guia06layout/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.guia06layout
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+    fun onClickFrame(v: View?) {
+        val llamar = Intent(this, FrameLayout::class.java)
+        startActivity(llamar)
+    }
+    fun onClickLinear(v: View?) {
+        val llamar = Intent(this, LinearLayout::class.java)
+        startActivity(llamar)
+    }
+    fun onClickRelative(v: View?) {
+        val llamar = Intent(this, RelativeLayout::class.java)
+        startActivity(llamar)
+    }
+    fun onClickTable(v: View?) {
+        val llamar = Intent(this, TableLayout::class.java)
+        startActivity(llamar)
+    }
+    fun onClickGrid(v: View?) {
+        val llamar = Intent(this, GridLayout::class.java)
+        startActivity(llamar)
     }
 }
